6f280e23a5c5942211346f674af55a25445786ad
Mb031 <matthewsblack03@gmail.com>
2019-05-19 00:05:36 -0400

    added the rest of the "you do it" files for the chapter

---
+++ Chapter 4/CarInsurancePolicy2.java
@@ -0,0 +1,19 @@
+public class CarInsurancePolicy2 {
+   private int policyNumber;
+   private int numPayments;
+   private String residentCity;
+   public CarInsurancePolicy2(int num, int payments, String city) {
+      policyNumber = num;
+      numPayments = payments;
+      residentCity = city;
+   }
+   public CarInsurancePolicy2(int num, int payments) {
+      this(num, payments, "Mayfield");
+   }
+   public CarInsurancePolicy2(int num) {
+      this(num, 2, "Mayfield");
+   }
+   public void display() {
+      System.out.println("Policy #" + policyNumber + ". " + numPayments + " payments annually. Driver resides in " + residentCity + ".");
+   }
+}
