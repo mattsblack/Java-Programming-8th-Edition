ab216c299db93ac3a3c508f537e3630633b6e2f8
Mb031 <matthewsblack03@gmail.com>
2019-05-26 17:48:05 -0400

    added two files

---
+++ Chapter 5/AssignVolunteer.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class AssignVolunteer {
+   public static void main(String[] args) {
+      int donationType;
+      String volunteer;
+      final int CLOTHING_CODE = 1;
+      final int OTHER_CODE = 2;
+      final String CLOTHING_PRICER = "Regina";
+      final String OTHER_PRICER = "Marco";
+      Scanner input = new Scanner(System.in);
+      System.out.println("What type of donationis this?");
+      System.out.println("Enter " + CLOTHING_CODE + " for clothing, " + OTHER_CODE + " for anything else...");
+      donationType = input.nextInt();
+      if(donationType == CLOTHING_CODE)
+         volunteer = CLOTHING_PRICER;
+      else
+         volunteer = OTHER_PRICER;
+      System.out.println("You entered " + donationType);
+      System.out.println("The volunteer who wil price this item is " + volunteer);
+   }
+}
---
+++ Chapter 5/AssignVolunteer2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class AssignVolunteer2 {
+   public static void main(String[] args) {
+      int donationType;
+      String volunteer;
+      final int CLOTHING_CODE = 1;
+      final int OTHER_CODE = 2;
+      final String CLOTHING_PRICER = "Regina";
+      final String OTHER_PRICER = "Marco";
+      String message;
+      Scanner input = new Scanner(System.in);
+      System.out.println("What type of donationis this?");
+      System.out.println("Enter " + CLOTHING_CODE + " for clothing, " + OTHER_CODE + " for anything else...");
+      donationType = input.nextInt();
+      if(donationType == CLOTHING_CODE) {
+         volunteer = CLOTHING_PRICER;
+         message = "a clothing donation";
+      }
+      else {
+         volunteer = OTHER_PRICER;
+         message = "a non-clothing donation";
+      }
+      System.out.println("You entered " + donationType);
+      System.out.println("The volunteer who wil price this item is " + volunteer);
+      System.out.println("This is " + message);
+   }
+}
