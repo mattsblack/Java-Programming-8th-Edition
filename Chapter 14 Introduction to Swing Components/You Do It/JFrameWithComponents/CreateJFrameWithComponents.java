5a3ab5d04a432b36ebdef0d45968e574425af596
Mb031 <matthewsblack03@gmail.com>
2023-04-17 20:15:49 -0400

    add ch 14 'you do it' exercises

---
+++ Chapter 14 Introduction to Swing Components/You Do It/JFrameWithComponents/CreateJFrameWithComponents.java
@@ -0,0 +1,11 @@
+package JFrameWithComponents;
+
+public class CreateJFrameWithComponents {
+    public static void main(String[] args) {
+        JFrameWithComponents aFrame = new JFrameWithComponents();
+        final int WIDTH = 350;
+        final int HEIGHT = 100;
+        aFrame.setSize(WIDTH, HEIGHT);
+        aFrame.setVisible(true);
+    }
+}
